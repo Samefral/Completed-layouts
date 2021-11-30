abca9328200cd0663370b4d1bb0f76905d4514f0
Samefral <galchun174@mail.ru>
2021-11-30 15:16:43 +0500

    added figma layouts, js functions and inner page for Edison Shoes

--- button-up/button-up-Encyclopedia/js/script.js
+++ button-up/button-up-Encyclopedia/js/script.js
@@ -1,7 +1,7 @@
 let upButton = document.querySelector(".up-button");
 
 window.onscroll = function () {
-  if (pageYOffset > 150) {
+  if (pageYOffset > 950) {
     upButton.classList.add("up-button-show");
   } else {
     upButton.classList.remove("up-button-show");
---
+++ cookie/cookie-GoodSchool/js/app.js
@@ -0,0 +1,10 @@
+let cookieNotice = document.querySelector(".cookie-notice");
+let cookieCloseBtns = document.querySelectorAll(".cookie-btn");
+for (let btn of cookieCloseBtns) {
+    btn.onclick = () => {
+        cookieNotice.style.opacity = 0; 
+        setTimeout(() => {
+            if (cookieNotice.style.opacity == 0) cookieNotice.remove();
+        }, 400);
+    }
+}
